e5dd44e78983ee826fc7c6f8277915e8b95332d3
Joseph Ero <erojoseph94@gmail.com>
2023-04-17 12:46:53 +0100

    posts resource

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger"
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+   const config = new DocumentBuilder()
+    .setTitle('Nest App')
+    .setDescription('The Nestjs Blogging API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(4000);
 }
 bootstrap();
