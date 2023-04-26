2c62cdb7752dfbdfbf4672a7fc6f7553927aa908
Joseph Ero <erojoseph94@gmail.com>
2023-04-26 14:30:46 +0100

    add requests validation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,6 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreatePostDto {
-  title: string
-  description?: string
-  body: string
-  published: boolean
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @ApiProperty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
+  @ApiProperty({ required: false })
+  description?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  body: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false, default: false })
+  published?: boolean = false;
 }
+
